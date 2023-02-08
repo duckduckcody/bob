4d07d63de6bcddb10c9414040c7e90a4b35f570b
jora-cody <cody.hennighan@jora.com>
2023-02-08 09:42:57 +0800

    work with seek

--- bob.js
+++ bob.js
@@ -1,7 +1,7 @@
-var fs = require('fs');
-const { exec } = require('child_process');
+var fs = require("fs");
+const { exec } = require("child_process");
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -9,15 +9,19 @@ const rl = readline.createInterface({
 
 const toPascalCase = (text) => text.replace(/(^\w|-\w)/g, clearAndUpper);
 
-const clearAndUpper = (text) => text.replace(/-/, '').toUpperCase();
+const clearAndUpper = (text) => text.replace(/-/, "").toUpperCase();
 
 const capitalize = (word) => word.charAt(0).toUpperCase() + word.substring(1);
 
 const toCapitalizedWords = (name) =>
-  (name.match(/[A-Za-z][a-z]*/g) || []).map(capitalize).join(' ');
+  (name.match(/[A-Za-z][a-z]*/g) || []).map(capitalize).join(" ");
 
 let componentsDir = undefined;
-const POTENTIAL_PATHS = ['./components', './src/components'];
+const POTENTIAL_PATHS = [
+  "./components",
+  "./src/components",
+  "./app/components",
+];
 // find the path to components dir
 POTENTIAL_PATHS.some((path) => {
   if (fs.existsSync(path)) {
@@ -28,54 +32,54 @@ POTENTIAL_PATHS.some((path) => {
 });
 
 // get component name from first script argument
-const kebabComponentName = process.argv[2];
-if (!kebabComponentName) {
-  console.error('Please provide a component name');
+const componentNameInput = process.argv[2];
+if (!componentNameInput) {
+  console.error("Please provide a component name");
   process.exit(0);
 }
 
 // get optional story dir from second script argument
 var storyDir = process.argv[3];
-if (storyDir === 'o') storyDir = 'organisms';
-if (storyDir === 'm') storyDir = 'modules';
-if (storyDir === undefined) storyDir = 'atoms';
+if (storyDir === "o") storyDir = "organisms";
+if (storyDir === "m") storyDir = "modules";
+if (storyDir === undefined) storyDir = "atoms";
 
 let figmaLink = undefined;
-rl.question('What is the figma link?: ', (link) => {
+rl.question("What is the figma link?: ", (link) => {
   figmaLink = link;
   rl.close();
 });
 
-rl.on('close', () => {
-  const componentName = toPascalCase(kebabComponentName);
-  const storybookName = toCapitalizedWords(componentName);
-  const componentFileName = `${componentsDir}/${kebabComponentName}/${kebabComponentName}.tsx`;
-  const storyFileName = `${componentsDir}/${kebabComponentName}/${kebabComponentName}.stories.tsx`;
+rl.on("close", () => {
+  const componentName = toPascalCase(componentNameInput);
+  const readableName = toCapitalizedWords(componentName);
+  const componentFileName = `${componentsDir}/${componentName}/${componentName}.tsx`;
+  const storyFileName = `${componentsDir}/${componentName}/${componentName}.stories.tsx`;
 
   try {
-    fs.mkdirSync(`${componentsDir}/${kebabComponentName}`);
+    fs.mkdirSync(`${componentsDir}/${componentName}`);
     fs.writeFileSync(
       componentFileName,
       [
         `import { FC } from 'react';\n`,
         `import styled from 'styled-components';\n`,
         `\n`,
-        'const Container = styled.div``;\n',
+        "const Container = styled.div``;\n",
         `\n`,
         `export interface ${componentName}Props {}\n`,
         `\n`,
         `export const ${componentName}: FC<${componentName}Props> = () => <Container></Container>;`,
-      ].join('')
+      ].join("")
     );
     fs.writeFileSync(
       storyFileName,
       [
         `import { Meta, Story } from '@storybook/react';\n`,
         `import { withDesign } from 'storybook-addon-designs';\n`,
-        `import { ${componentName}, ${componentName}Props } from './${kebabComponentName}';\n`,
+        `import { ${componentName}, ${componentName}Props } from './${componentName}';\n`,
         `\n`,
         `export default {\n`,
-        `  title: '${storyDir}/${storybookName}',\n`,
+        `  title: '${storyDir}/${readableName}',\n`,
         `  component: ${componentName},\n`,
         `  decorators: [withDesign],\n`,
         `  parameters: {\n`,
@@ -89,9 +93,9 @@ rl.on('close', () => {
         `const Template: Story<${componentName}Props> = (args) => <${componentName} {...args} />;\n`,
         `export const Primary = Template.bind({});\n`,
         `Primary.args = {};`,
-      ].join('')
+      ].join("")
     );
-    console.log('🔨 files made 🔨');
+    console.log("🔨 files made 🔨");
 
     // open files when completed
     exec(`code ./${storyFileName} && code ./${componentFileName}`);
